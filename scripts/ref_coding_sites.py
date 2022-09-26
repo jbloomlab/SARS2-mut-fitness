59b32c39a8dde5d20b2c35a9d787727c7accd0bb
jbloom <jbloom@fredhutch.org>
2022-09-26 14:46:58 -0700

    annotate ref coding sites by codon position

--- scripts/ref_coding_sites.py
+++ scripts/ref_coding_sites.py
@@ -3,19 +3,59 @@
 
 import pandas as pd
 
+import re
 
+
+# get CDS features
+cds_features = (
+    pd.read_csv(
+        snakemake.input.gtf,
+        sep="\t",
+        header=None,
+        names=[
+            "seqName",
+            "source",
+            "feature",
+            "start",
+            "end",
+            "score",
+            "strand",
+            "frame",
+            "attribute",
+        ],
+    )
+    .query("feature == 'CDS'")
+    .assign(
+        frame=lambda x: x["frame"].astype(int),
+        gene=lambda x: x["attribute"].str.extract('^gene_id "(\w+)"'),
+    )
+    .drop(columns=["seqName", "source", "score", "feature", "attribute"])
+)
+
+assert all(cds_features["frame"] == 0), "non frame 0 CDSs\n" + str(cds_features)
+assert all(cds_features["strand"] == "+"), "non + sense CDS\n" + str(cds_features)
+
+# get sites with codon position assigned
 sites = (
-    pd.read_csv(snakemake.input.gtf, sep="\t", header=None)
-    .rename(columns={2: "feature_type", 3: "start", 4: "end"})
-    [["feature_type", "start", "end"]]
-    .query("feature_type == 'CDS'")
+    cds_features
     .assign(
         site=lambda x: x.apply(lambda r: list(range(r["start"], r["end"] + 1)), axis=1),
     )
-    .explode("site")
-    ["site"]
-    .drop_duplicates()
-    .sort_values()
+    .explode("site", ignore_index=True)
+    .assign(
+        codon_position=lambda x: x.apply(
+            lambda r: (r["site"] - r["start"]) % 3 + 1, axis=1,
+        ),
+    )
+    .drop(columns=["strand", "frame", "start", "end"])
+    .groupby("site", as_index=False)
+    .aggregate(
+        codon_position=pd.NamedAgg(
+            "codon_position",
+            lambda s: s.values[0] if s.nunique() == 1 else "mixed",
+        ),
+        gene=pd.NamedAgg("gene", lambda s: ";".join(s)),
+    )
 )
 
 sites.to_csv(snakemake.output.csv, index=False)
