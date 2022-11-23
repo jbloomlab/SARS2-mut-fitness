9f52c389b571f5bcef7c0b8325e6e43ef3482fc0
kbfeldmann <kbfeldmann@gmail.com>
2022-11-22 16:57:29 -0800

    Decrease distant between points and labels, fix plot dimensions and assign colors to genes

--- expected_and_actual_counts_ratio/scripts/volcano_plot.R
+++ expected_and_actual_counts_ratio/scripts/volcano_plot.R
@@ -1,8 +1,12 @@
+library(stringr)
 library(ggplot2)
 library(RColorBrewer)
 library(grid)
 
 `%notin%` <- Negate(`%in%`)
+
+# Determine what the clades being compared are.
+clades <- str_split(snakemake@wildcards[["comparisons"]], "_", 2)[[1]]
 
 # Specify column for plotting mutation data (nt_mutation or aa_mutation).
 merge_column <- paste(snakemake@wildcards[["mutation_type"]], "mutation", sep="_")
@@ -14,15 +18,16 @@ input_name <- paste("results/pvalues", snakemake@wildcards[["comparisons"]], "_"
 comparison_p_values <- read.csv(input_name, sep=",", header=TRUE)
 
 ## After dropping P = 1, plot the corrected p-values. ##
-volcano <- comparison_p_values[which(comparison_p_values$p_value_corrected!=1),]
-color <- colorRampPalette(brewer.pal(8,"Set1"))(length(unique(volcano$gene)))
 
-neg_clade <- textGrob(unique(volcano$clade_2), gp=gpar(fontsize=13, fontface="bold"))
-pos_clade <- textGrob(unique(volcano$clade_1), gp=gpar(fontsize=13, fontface="bold"))
+all_colors <- data.frame(color=colorRampPalette(brewer.pal(8,"Set1"))(length(unique(comparison_p_values$gene))), gene=levels(as.factor(comparison_p_values$gene)))
+volcano <- comparison_p_values[which(comparison_p_values$p_value_corrected!=1),]
+color <- all_colors[all_colors$gene %in% unique(volcano$gene), "color"]
+
+neg_clade <- textGrob(clades[2], gp=gpar(fontsize=20, fontface="bold"))
+pos_clade <- textGrob(clades[1], gp=gpar(fontsize=20, fontface="bold"))
 
 na <- ggplot(volcano, aes(x=fold_change, y=-log10(p_value_corrected), label=get(merge_column), color=gene))+
   geom_point()+
-  geom_text(color="black", size=2, nudge_y=0.2)+
   geom_hline(yintercept=-log10(0.05), col="red", linetype=2)+
   scale_color_manual(values=color, name="Mutation")+
   theme_bw()+
@@ -31,15 +36,15 @@ na <- ggplot(volcano, aes(x=fold_change, y=-log10(p_value_corrected), label=get(
 x <- layer_scales(na)$x$range$range
 y <- layer_scales(na)$y$range$range
 
-pdf(file=snakemake@output[[1]])
+pdf(file=snakemake@output[[1]], width=12, height=10)
 
 if(is.null(x)){
   na
 } else {
-  p <- na + coord_cartesian(xlim=c(-max(abs(x)), max(abs(x))))
+  p <- na + coord_cartesian(xlim=c(-max(abs(x)), max(abs(x)))) + geom_text(color="black", size=3, nudge_y=diff(y)/50)
   x_mod <- p$coordinates$limits[[1]]
-  
-  p + annotation_custom(neg_clade,xmin=x_mod[1]*0.7,xmax=x_mod[1]*0.7,ymin=y[2]*0.95,ymax=y[2]*0.95)+ 
+
+  p + annotation_custom(neg_clade,xmin=x_mod[1]*0.7,xmax=x_mod[1]*0.7,ymin=y[2]*0.95,ymax=y[2]*0.95)+
       annotation_custom(pos_clade,xmin=x_mod[2]*0.7,xmax=x_mod[2]*0.7,ymin=y[2]*0.95,ymax=y[2]*0.95)
 }
 
